c40b8c14e6be10696933843812248c9d99ca8092
Zahid Ullah <zahiduullah2@gmail.com>
2026-04-16 10:27:51 -0700

    add secondfile.java with initial implementation

---
+++ secondfile.java
@@ -0,0 +1,8 @@
+public class secondfile {
+    public static void main(String[] args) {
+        int[] arr = {10, 20, 30, 40, 50};
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
